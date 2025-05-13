daf2499946bf709ced3151b6fca995690c798e23
Flora2401 <espinozapacaflora@gmail.com>
2025-05-12 22:12:22 -0400

    arreglar orden iniciar sesion variable

--- web/iniciarSesion.php
+++ web/iniciarSesion.php
@@ -31,7 +31,7 @@
 
 <?php
     
-    if ($_POST['iniciarSesion'] && isset($_POST['iniciarSesion'])) {
+    if (isset($_POST['iniciarSesion']) && $_POST['iniciarSesion']) {
         
         $username = $_POST['username'];
         $password = $_POST['password'];
