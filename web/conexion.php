7ed0282e9694e98a19f41ce113a15bce3c6c5d91
Shamir <shamirteranmustafa@gmail.com>
2025-06-11 12:03:07 -0400

    fix: arreglar errores de tildes

--- web/conexion.php
+++ web/conexion.php
@@ -1,6 +1,7 @@
 <?php
 
 $conn = new mysqli("localhost", "shamir", "", "reciclaje");
+$conn->set_charset("utf8mb4");
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
