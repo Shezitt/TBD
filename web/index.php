65f58820cf874c1bb2cc7c156efc2a7791d8c459
Shezitt <shamirteranmustafa@gmail.com>
2025-06-18 15:03:33 -0400

    fix promociones: mostrar solo las que no fueron eliminadas

--- web/index.php
+++ web/index.php
@@ -302,7 +302,7 @@ $datosUsuario = $datosUsuario->fetch_assoc();
                 <h2>Promociones</h2>
                 <?php
 
-                $stmt = $conn->prepare('SELECT * FROM Promocion;');
+                $stmt = $conn->prepare('SELECT * FROM Promocion WHERE activo=1;');
                 $stmt->execute();
                 $resultado = $stmt->get_result();
                 $stmt->close();
